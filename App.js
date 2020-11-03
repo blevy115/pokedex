37b05f889a9c6f8a27131f5c53e7bab7b2c83386
Brandon Levy <brandon.levy@mail.mcgill.ca>
2020-11-03 18:44:27 -0500

    Added React Native AutoComplete that filters pokemon by whats in the Input and Can be touched to perform the search

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, Text, View, TouchableOpacity, SafeAreaView } from 'react-native';
 import { SearchBar } from 'react-native-elements';
+import Autocomplete from 'react-native-autocomplete-input';
 import useResults from './hooks/useResults';
 import SearchTile from './src/components/search-tile';
 import pokeapi from './api/pokeapi';
@@ -11,6 +12,7 @@ export default function App() {
 
   const [search, setSearch] = useState("");
   const [pkmList, setPkmList] = useState([]);
+  const [filteredPkmList, setFilteredPkmList] = useState([]);
   const [searchApi, results, error] = useResults();
 
   const loadPkmList = async () => {
@@ -22,17 +24,62 @@ export default function App() {
     loadPkmList();
   }, []);
 
+  const findPkm = (query) => {
+    if (query) {
+      setSearch(query);
+      const regex = new RegExp(`${query.trim()}`, 'i');
+      setFilteredPkmList(
+        pkmList.filter((pkm) => pkm.search(regex) >= 0)
+      );
+    } else {
+      setFilteredPkmList([]);
+    }
+  }
+
+
   return (
+    <SafeAreaView style={{flex: 1}}>
     <View style={styles.container}>
       <Text>My Pokedex App</Text>
-      <SearchBar
-        platform = "ios"
-        placeholder="Search"
-        onChangeText={(term) => setSearch(term)}
-        onSubmitEditing={() => searchApi(search)}
-        value={search}
-        autoComplete = {false}
-      />
+      <Autocomplete
+          autoCapitalize="none"
+          autoCorrect={false}
+          containerStyle={styles.autocompleteContainer}
+          //data to show in suggestion
+          data={filteredPkmList}
+          //default value if you want to set something in input
+          defaultValue={
+            JSON.stringify(search) === '{}' ?
+            '' :
+            search
+          }
+          // onchange of the text changing the state of the query
+          // which will trigger the findFilm method
+          // to show the suggestions
+          // listContainerStyle={{flex:1}}
+          keyExtractor = {item => filteredPkmList.indexOf(item).toString()}
+          listStyle={{maxHeight:300}}
+          onChangeText={(text) => findPkm(text)}
+          onSubmitEditing={() => {
+            searchApi(search)
+            setSearch('')
+            setFilteredPkmList([]);
+          }}
+          placeholder="Enter Pokemon Name"
+          renderItem={({item}) => (
+            //you can change the view you want to show in suggestions
+            <TouchableOpacity
+              onPress={() => {
+                searchApi(item);
+                setSearch('')
+                setFilteredPkmList([]);
+              }}>
+              <Text style={styles.itemText}>
+                  {item}
+              </Text>
+            </TouchableOpacity>
+          )}
+        />
       {results.name ? (
         <SearchTile pokemon={results} />
       ) : (
@@ -40,15 +87,44 @@ export default function App() {
       )}
       <StatusBar style="auto" />
     </View>
+    </SafeAreaView>
   );
 }
 
+
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: '#F5FCFF',
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'flex-start',
-    marginTop: 100
+    padding: 16,
+    marginTop: 40,
+  },
+  autocompleteContainer: {
+    backgroundColor: '#ffffff',
+    borderWidth: 0,
+  },
+  descriptionContainer: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  itemText: {
+    fontSize: 15,
+    paddingTop: 5,
+    paddingBottom: 5,
+    margin: 2,
+  },
+  infoText: {
+    textAlign: 'center',
+    fontSize: 16,
   },
 });
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'flex-start',
+//     marginTop: 100
+//   },
+// });
